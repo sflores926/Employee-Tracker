dd969a42b9e96312e02807f022968566909d4c14
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-14 12:58:39 -0400

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -23,6 +23,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
+    manager_id INT, 
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
