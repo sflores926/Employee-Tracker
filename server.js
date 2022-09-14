1ecd89f6b410b31a9b2cfdad95b8c45f2feb34b8
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-14 18:46:35 -0400

    started to work on the commection to the database

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 //import
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
-const conTable = require('console.table')
+const cTable = require('console.table')
+
+require('dotenv').config();
+
+//To connect to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employee_db',
+});
+
+
 
